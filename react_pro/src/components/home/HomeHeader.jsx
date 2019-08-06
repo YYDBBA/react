896971a52ef9737c6ddd0a12c475524e72104ac2
YYDBBA <1806254330@qq.com>
2019-08-06 13:02:26 +0800

    add home

---
+++ react_pro/src/components/home/HomeHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './home.css'
+
+class HomeHeader extends React.Component{
+    render(){
+        return <div className="home-header">我是头部</div>
+    }
+}
+export default HomeHeader;
---
+++ react_pro/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeHeader from './../components/home/HomeHeader.jsx'
+
+class Home extends React.Component{
+    // constructor(){
+    //     super();
+    // }
+    render(){
+        return <HomeHeader />
+    }
+}
+
+export default Home;
